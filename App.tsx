ce78ae87b5fffb4282008533038d737b31a15d7c
Olivier Bouillet <olivier@azzapp.com>
2025-02-04 16:15:49 +0100

    fix: simplify sample

--- App.tsx
+++ App.tsx
@@ -5,113 +5,62 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useEffect, useState } from 'react';
 import {
+  Pressable,
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import {Screen, ScreenStack} from 'react-native-screens';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [childDisplayed, setChildDisplayed] = useState(false);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  // autoclose the view
+  useEffect(() => {
+    if (childDisplayed) {
+      setTimeout(() => {
+        setChildDisplayed(false);
+      }, 5000);
+    }
+  }, [childDisplayed]);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={{ backgroundColor: 'white', height: '100%' }}>
+    <ScreenStack style={StyleSheet.absoluteFill}>
+      <Screen
+        key="parent"
+        activityState={2}
+        style={StyleSheet.absoluteFill}>
+        <Pressable
+            style={styles.pressable}
+            onPress={() => {
+              setChildDisplayed(true);
+            }}/>
+      </Screen>
+      {childDisplayed ? (
+        <Screen
+          key="child"
+          activityState={2}
+          transitionDuration={10000}
+          isNativeStack
+          stackAnimation={'slide_from_bottom'}
+          style={StyleSheet.absoluteFill}>
+            <View style={styles.square}/>
+        </Screen>) :
+        <></> }
+    </ScreenStack>
     </SafeAreaView>
   );
 }
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  pressable: { width: 100, height: 100, backgroundColor: 'red' },
+  square: {
+    height: '100%',
+    overflow: 'hidden', // removing hidden fix the issue
+    backgroundColor: 'green',
   },
 });
 
